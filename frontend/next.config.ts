6d6828d1f13068650208fcf3211899df0458a616
yashovardhan3304 <159131944+yashovardhan3304@users.noreply.github.com>
2025-09-17 19:13:41 +0530

    Update next.config.ts

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,13 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-};
-module.exports = {
   eslint: {
     ignoreDuringBuilds: true,
   },
+  // other config options here...
 };
 
-
 export default nextConfig;
+
